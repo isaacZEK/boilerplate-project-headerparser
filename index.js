a9fc823508a3393ea167581c624869a4761147cf
isaacZEK <isaac.chama32@gmail.com>
2024-11-30 06:50:20 +0200

    Completed challeng: header-parser

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 // index.js
 // where your node app starts
 
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// enable CORS for FCC testing
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // legacy browsers may choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// serve static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+// serve index.html for the root route
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
+// API endpoint for greeting
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+// API endpoint for header parser
+app.get('/api/whoami', (req, res) => {
+  // Extract necessary information from request headers
+  const ipaddress = req.headers['x-forwarded-for']?.split(',')[0] || req.ip; // client's IP
+  const language = req.headers['accept-language']; // client's language
+  const software = req.headers['user-agent']; // client's user-agent
+
+  // Respond with extracted data
+  res.json({
+    ipaddress,
+    language,
+    software,
+  });
+});
+
+// start listening for requests
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
